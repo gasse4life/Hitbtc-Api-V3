c1464a5c10c6fc373d9c413a94b10aa5a43aa126
gasse4life <s.gasse@mail.com>
2021-11-05 17:19:49 -0400

    update 2.1

--- module/footer.php
+++ module/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2020-2021 <a href="À">Depanneur-Virtuel INC.</a>.</strong>
+    <strong>Copyright &copy; 2020-2021 Depanneur-Virtuel INC. </strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.1.12
